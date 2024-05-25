241a2c09d9f05c5c6ff5fc5e24ade75f4b2ca5b6
Jon Palmer <nextgenusfs@gmail.com>
2024-05-24 20:23:43 -0700

    try to fix cert/issue with jgi taxonomy lookup

--- funannotate2/utilities.py
+++ funannotate2/utilities.py
@@ -14,6 +14,7 @@ import uuid
 from concurrent.futures import ThreadPoolExecutor
 from threading import Lock
 import requests
+import urllib3
 import errno
 from urllib.request import urlopen
 import socket
@@ -92,12 +93,13 @@ def lookup_taxonomy(name):
         returns False if not a valid name else returns dictionary of taxonomic levels/names
 
     """
-
+    requests.packages.urllib3.disable_warnings()
+    requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS += ":HIGH:!DH:!aNULL"
     if " " in name:
         name = name.replace(" ", "_")
     url = "https://taxonomy.jgi-psf.org/stax/name/"
     try:
-        resp = requests.get(url + name)
+        resp = requests.get(url + name, verify=False)
         if 500 <= resp.status_code <= 599:
             return resp.status_code
         result = resp.json()[name]
